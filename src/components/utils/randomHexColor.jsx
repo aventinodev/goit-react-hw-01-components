c878a3ef77aa16a91ec5b1f1c709c57424e46d7a
Olena Bondar <olena.bondar1060@gmail.com>
2023-01-09 22:26:55 +0200

    Update randomHexColor.jsx

--- src/components/utils/randomHexColor.jsx
+++ src/components/utils/randomHexColor.jsx
@@ -1,6 +1,25 @@
 export default function getRandomHexColor() {
-  let randomColor = `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, '0')}`;
-  return randomColor;
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += Math.floor(Math.random() * 16).toString(16);
+  }
+  if (getLuminance(color) > 0.354) {
+    return getRandomHexColor();
+  }
+  return color;
 }
+
+function getLuminance(color) {
+  let r = parseInt(color.substring(1, 3), 16) / 255;
+  let g = parseInt(color.substring(3, 5), 16) / 255;
+  let b = parseInt(color.substring(5, 7), 16) / 255;
+  let luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  return luminance;
+}
+
+// function getRandomHexColor() {
+//   let randomColor = `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, '0')}`;
+//   return randomColor;
+// }
